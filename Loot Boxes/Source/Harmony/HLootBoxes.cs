1213a340541c22c42362ec4d7fd408b30c86af3a
BWiggins <60398078+bwiggins10@users.noreply.github.com>
2020-04-18 19:07:25 -0400

    Add files via upload

---
+++ Loot Boxes/Source/Harmony/HLootBoxes.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace LootBoxes
+{
+
+    [StaticConstructorOnStartup]
+    public static class HLootBoxes
+    {
+
+        static HLootBoxes()
+        {
+            Harmony harmony = new Harmony("LootBoxesUpdate_Ben");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+
+}
