b7c46e1ac5c2be5f42a923f83e15bbeccce05886
bwiggins10 <benjimouse2000@gmail.com>
2020-08-13 09:43:35 -0400

    1.2

--- NoRandomConstructionQuality/Source/NoRandomConstructionQuality/_QualityUtility.cs
+++ NoRandomConstructionQuality/Source/NoRandomConstructionQuality/_QualityUtility.cs
@@ -6,56 +6,47 @@ namespace NoRandomConstructionQuality
 {
 	public static class _QualityUtility
 	{
-		
-
 		public static QualityCategory GetStaticQuality(int relevantSkillLevel)
 		{
-			if (Nrcq_mod.settings.custom_quality == 0)
+			switch (relevantSkillLevel)
 			{
-				switch (relevantSkillLevel)
-				{
-					case 0:
-					case 1:
-					case 2:
-					case 3:
-						return QualityCategory.Awful;
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+					return QualityCategory.Awful;
 
-					case 4:
-					case 5:
-					case 6:
-					case 7:
-						return QualityCategory.Poor;
+				case 4:
+				case 5:
+				case 6:
+				case 7:
+					return QualityCategory.Poor;
 
-					case 8:
-					case 9:
-					case 10:
-						return QualityCategory.Normal;
+				case 8:
+				case 9:
+				case 10:
+					return QualityCategory.Normal;
 
-					case 11:
-					case 12:
-					case 13:
-						return QualityCategory.Good;
+				case 11:
+				case 12:
+				case 13:
+					return QualityCategory.Good;
 
-					case 14:
-					case 15:
-					case 16:
-						return QualityCategory.Excellent;
+				case 14:
+				case 15:
+				case 16:
+					return QualityCategory.Excellent;
 
-					case 17:
-					case 18:
-					case 19:
-						return QualityCategory.Masterwork;
+				case 17:
+				case 18:
+				case 19:
+					return QualityCategory.Masterwork;
 
-					case 20:
-						return QualityCategory.Legendary;
+				case 20:
+					return QualityCategory.Legendary;
 
-					default:
-						throw new InvalidOperationException("Unable to determine quality for skill level " + relevantSkillLevel);
-				}
-			}
-			else
-			{
-			
+				default:
+					throw new InvalidOperationException("Unable to determine quality for skill level " + relevantSkillLevel);
 			}
 		}
 
@@ -77,10 +68,12 @@ namespace NoRandomConstructionQuality
 		}
 
 
+
 		public static void _SendCraftNotification(Thing thing, Pawn worker)
 
 		{
-			if (worker == null)
+			Nrcq_settings cnSetting = new Nrcq_settings();
+			if (worker == null || cnSetting.crafting_notification == false)
 			{
 				return;
 			}
